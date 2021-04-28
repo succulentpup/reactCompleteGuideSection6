33879b32719d42cf594842247f5e05569be6fb6c
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-04-28 12:05:28 +0100

    media query is used

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 // };
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -15,17 +16,21 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  
+  @media(min-width: 680px) {
+    width: auto;
+  }
 }
 
-&:focus {
-  outline: none;
-}
+  &:focus {
+    outline: none;
+  }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 `
 
 export default Button;
